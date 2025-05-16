4a9ea64f88265df0666aff6178b2dc51016845e5
buison1602 <buicongson162@gmail.com>
2025-05-16 09:51:20 +0700

    add activity exam, class question, drawable

---
+++ app/src/main/java/com/example/onlineexams/Exam.java
@@ -0,0 +1,113 @@
+package com.example.onlineexams;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Exam extends AppCompatActivity {
+
+    private Question[] data;
+    private String QuizID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_exam);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // -------------------------------------------------------------------------------------
+        ListView listview = findViewById(R.id.listview);
+        Button submit = findViewById(R.id.submit);
+        TextView title = findViewById(R.id.title);
+
+    }
+
+    public class ListAdapter extends BaseAdapter {
+        Question[] arr;
+
+        ListAdapter(Question[] arr2) {
+            arr = arr2;
+        }
+
+        @Override
+        public int getCount() {
+            return arr.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return arr[i];
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return i;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            LayoutInflater inflater = getLayoutInflater();
+            View v = inflater.inflate(R.layout.question, null);
+
+            TextView question = v.findViewById(R.id.question);
+            RadioButton option1 = v.findViewById(R.id.option1);
+            RadioButton option2 = v.findViewById(R.id.option2);
+            RadioButton option3 = v.findViewById(R.id.option3);
+            RadioButton option4 = v.findViewById(R.id.option4);
+
+            question.setText(data[i].getQuestion());
+            option1.setText(data[i].getOption1());
+            option2.setText(data[i].getOption2());
+            option3.setText(data[i].getOption3());
+            option4.setText(data[i].getOption4());
+
+            option1.setOnCheckedChangeListener((CompoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(1);
+            });
+            option2.setOnCheckedChangeListener((CompoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(2);
+            });
+            option3.setOnCheckedChangeListener((CompoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(3);
+            });
+            option4.setOnCheckedChangeListener((CompoundButton, b) -> {
+                if (b) data[i].setSelectedAnswer(4);
+            });
+
+            switch ( data[i].getSelectedAnswerAnswer() ) {
+                case 1:
+                    option1.setChecked(true);
+                    break;
+                case 2:
+                    option1.setChecked(true);
+                    break;
+                case 3:
+                    option1.setChecked(true);
+                    break;
+                case 4:
+                    option1.setChecked(true);
+                    break;
+            }
+            return v;
+        }
+    }
+}
